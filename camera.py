b9a7dbd05f97da69986d313202f3768fbf2a603b
Mihir Suvarna <49384703+msuv08@users.noreply.github.com>
2022-01-06 21:09:35 -0600

    Added camera script, included init & del functions

---
+++ camera.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+
+class Camera:
+
+    def __init__(self):
+        # Access camera module from connected computer (0 for integrated cam)
+        self.camera = cv.VideoCapture(0)
+        # Check if camera access was valid, raise error if not
+        if self.camera.isOpened():
+            self.width = self.camera.get(cv.CAP_PROP_FRAME_WIDTH)
+            self.height = self.camera.get(cv.CAP_PROP_FRAME_HEIGHT)
+        else:
+            raise ValueError("Not able to access camera.")
+    
+    def __del__(self):
+        # Close camera on object deletion
+        if self.camera.isOpened():
+            self.camera.release()
